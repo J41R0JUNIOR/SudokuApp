e3a537335ea7d5712f95c2e9bf93b2a4a790990a
Jairo Júnior <81876783+J41R0JUNIOR@users.noreply.github.com>
2024-09-01 20:26:33 -0300

    fixing bugs

--- Sudoku/Sudoku/Model/Controls/ThemeManager.swift
+++ Sudoku/Sudoku/Model/Controls/ThemeManager.swift
@@ -9,14 +9,14 @@ import Foundation
 import SwiftUI
 
 class ThemeManager: ObservableObject {
-    @AppStorage(EnumModel.darkMode.rawValue) var isDarkMode: Bool = false {
+    @AppStorage(UserDefaultsKeysToSave.darkMode.rawValue) var isDarkMode: Bool = false {
         didSet {
-            UserDefaults.standard.set(isDarkMode, forKey: EnumModel.darkMode.rawValue)
+            UserDefaults.standard.set(isDarkMode, forKey: UserDefaultsKeysToSave.darkMode.rawValue)
         }
     }
 
     init() {
-        if let savedTheme = UserDefaults.standard.value(forKey: EnumModel.darkMode.rawValue) as? Bool {
+        if let savedTheme = UserDefaults.standard.value(forKey: UserDefaultsKeysToSave.darkMode.rawValue) as? Bool {
             self.isDarkMode = savedTheme
         }
     }
